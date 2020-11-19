179c0728036ae6b8f5e2ab9cd7381e6a03292145
Maja Gedda <maja.gedda@zocom.se>
2020-11-19 14:12:24 +0100

    house url

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
 let url = 'https://www.potterapi.com/v1/';
+let houseUrl = 'http://hp-api.herokuapp.com/api/characters/house/';
 let endPoint = '';
+let dataSet;
 let house = '';
+let spell = '';
+let patronus = '';
+let deatheater = false;
 
 let doMagic = () => {
     fetch(url + endPoint)
     .then(response => response.json())
     .then(data => {
-        house = data;
-        console.log(house);
+        dataSet = data;
     })
+    .catch((error) => {
+        console.error('Error: ', error);
+    });
 }
 
 document.getElementById('sorting-hat-image').addEventListener('click', function() {
     endPoint = 'sortingHat';
-
     doMagic();
-    
+    house = dataSet;
+    document.getElementById('house').innerHTML = house;
 })
 
 document.getElementById('spells-image').addEventListener('click', function() {
     endPoint = 'spells';
+    doMagic();
+    spell = dataSet;
 })
