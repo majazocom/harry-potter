c3cdbfcb10d7ee96508750ccac455850332adecf
Maja Gedda <maja.gedda@zocom.se>
2020-11-19 13:41:00 +0100

    fetch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 let url = 'https://www.potterapi.com/v1/';
+let endPoint = '';
+let house = '';
+
+let doMagic = () => {
+    fetch(url + endPoint)
+    .then(response => response.json())
+    .then(data => {
+        house = data;
+        console.log(house);
+    })
+}
+
+document.getElementById('sorting-hat-image').addEventListener('click', function() {
+    endPoint = 'sortingHat';
+
+    doMagic();
+    
+})
+
+document.getElementById('spells-image').addEventListener('click', function() {
+    endPoint = 'spells';
+})
